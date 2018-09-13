98c9a3178651c3f347b061f87120a49bcb37e0a2
zero <sontt246@gmail.com>
2018-09-13 11:20:05 +0700

    Update config auth nodered

--- nodered/src/start.sh
+++ nodered/src/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo $ADMIN_PWD
-export ADMIN_PWD=`node -e "console.log(require('bcryptjs').h
-ashSync(process.argv[1], 8));" $ADMIN_PWD`
+export ADMIN_PWD=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" $ADMIN_PWD`
 echo $ADMIN_PWD
 npm start -- --userDir ./data
