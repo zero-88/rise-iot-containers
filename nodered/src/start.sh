8eeefbbd13f80af64bfbdeb69e0e97532c6a98c8
zero <sontt246@gmail.com>
2018-09-13 12:45:38 +0700

    Update auth nodered

--- nodered/src/start.sh
+++ nodered/src/start.sh
@@ -3,6 +3,14 @@ echo $NODE_RED_USER
 echo $NODE_RED_PASSWORD
 echo $ADMIN_USER
 echo $ADMIN_PWD
-export ADMIN_PWD=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" $ADMIN_PWD`
-echo $ADMIN_PWD
+[[ -z $ADMIN_USER ]] && { echo "Missing required user"; exit 1; }
+if [[ -z $ADMIN_PWD ]] ; then
+    if [[ -n $NODE_RED_PASSWORD ]]; then
+        export ADMIN_PWD=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" $NODE_RED_PASSWORD`
+        echo $ADMIN_PWD
+    else
+        echo "Missing required password"
+        exit 1;
+    fi
+fi
 npm start -- --userDir ./data
