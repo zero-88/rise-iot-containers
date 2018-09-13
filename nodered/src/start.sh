b2a0a4d93e1b20106ab4e7b17c52dc35e70a0c64
zero <sontt246@gmail.com>
2018-09-13 12:22:50 +0700

    Test auth

--- nodered/src/start.sh
+++ nodered/src/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo $NODE_RED_USER
+echo $NODE_RED_PASSWORD
+echo $ADMIN_USER
 echo $ADMIN_PWD
 export ADMIN_PWD=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" $ADMIN_PWD`
 echo $ADMIN_PWD
