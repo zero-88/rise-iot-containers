241ae70a3e7cb0c795c6ba3e7959a201bfb5cc87
zero <sontt246@gmail.com>
2018-09-16 13:11:11 +0700

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
 source ./.env
 source ./.secret.env
-DOCKER_API_VERSION=1.22 DOCKER_HOST=${LOCAL_RESIN_DOCKER_URL} docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
+cmd="DOCKER_API_VERSION=1.22 DOCKER_HOST=${LOCAL_RESIN_DOCKER_URL} docker-compose -f docker-compose.yml -f docker-compose.dev.yml $1"
+echo $cmd
+eval $cmd
