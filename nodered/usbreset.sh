68565e285ae8e062f5a52c40e86dd8c2dd5d6db0
zero <sontt246@gmail.com>
2018-09-23 11:10:57 +0700

    Update usbreset

--- nodered/usbreset.sh
+++ nodered/usbreset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-[[ -z $USB_VENDOR_ID || $USB_VENDOR_PRODUCT ]] && { echo "Missing USB environment: 'USB_VENDOR_ID' or 'USB_VENDOR_PRODUCT'"; exit 1; }
-usbpath=`lsusb | grep "$USB_VENDOR_ID:$USB_VENDOR_PRODUCT" | awk '{print $2"/"$4}' | sed 's/.$//'`
+[[ -z $1 ]] && device_id=$USB_VENDOR_ID || device_id=$1
+[[ -z $2 ]] && device_product=$USB_VENDOR_PRODUCT || device_product=$2
+[[ -z $device_id || -z $device_product ]] && { echo "Missing USB environment: 'USB_VENDOR_ID' or 'USB_VENDOR_PRODUCT'"; exit 1; }
+usbpath=`lsusb | grep "$device_id:$device_product" | awk '{print $2"/"$4}' | sed 's/.$//'`
 [[ -z $usbpath ]] && { echo "Not found USB"; exit 2; }
 usbreset /dev/bus/usb/$usbpath
